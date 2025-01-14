605127ae287198e112bdff5902da87f5730745ab
smallDazza <smalldazza@gmail.com>
2025-01-14 19:50:53 +1100

    Feat: express setup and response

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # intro-to-mongoosejs
+- MongooseJS
+mongosh - mongo shell
+mongod - mongo service daemon
+mongos - sharding or clustering for mongodb
---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Purpose of this file
+
+// First point of entry
+// Initialise the server
+// Get the port
+// Tell the server to listen to incoming traffic
+
+// Server is configured in this file
+const { app } = require("./server.js");
+
+//Get the PORT
+const PORT = process.env.PORT || 3005;
+
+//Listen to the PORT
+app.listen(PORT, () => {
+    // Server is running at this point
+    console.log("Server is running on port: " + PORT);
+});
+
+
+
